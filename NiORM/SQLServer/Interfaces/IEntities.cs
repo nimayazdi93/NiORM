11889c5cdbc78bbc9c36ec5bb939205cda6286a1
nima.yazdi <nima.yazdi93@gmail.com>
2024-09-23 19:41:09 +0330

    fix some errors

--- NiORM/SQLServer/Interfaces/IEntities.cs
+++ NiORM/SQLServer/Interfaces/IEntities.cs
@@ -3,7 +3,8 @@
     public interface IEntities<T> where T : ITable
     {
         List<T> List();
-        T Add(T entity);
+        T AddReturn(T entity);
+        void Add(T entity); 
         void Edit(T entity);
         void Remove(T entity);
     }
