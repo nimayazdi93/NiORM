881ece18276ffe683054d2954a8af12d1e8a9d9b
nima.yazdi <nima.yazdi93@gmail.com>
2024-10-02 09:21:11 +0330

    Add Execute & List to IEntities

--- NiORM/SQLServer/Interfaces/IEntities.cs
+++ NiORM/SQLServer/Interfaces/IEntities.cs
@@ -19,6 +19,8 @@ namespace NiORM.SQLServer.Interfaces
         T Find(string id);
         T FirstOrDefault(string Query);
         T FirstOrDefault();
+        void Execute(string Query);
+        List<T> List();
 
     }
 }
